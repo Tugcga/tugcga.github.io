ce7aa7db748c939e15ac9901f337696ace24d808
Shekn <cgproggame@gmail.com>
2021-02-14 15:22:06 +0500

    add playcanvas test apps

---
+++ test_apps/webgl/webgl_pc_instances/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1090762.json";
+CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 42562093, 42562458 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: false,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: false
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
